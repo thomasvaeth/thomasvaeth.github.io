a9b524e332e968bd6b0dc886977d69303033f7fb
thomasvaeth <thomas.vaeth@gmail.com>
2018-05-09 20:26:03 -0700

    Deleted old blog posts.

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,24 @@
-'use strict';
-
 const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
 const babelify = require('babelify');
 const browserify = require('browserify');
 const buffer = require('vinyl-buffer');
 const cleanCSS = require('gulp-clean-css');
+const eslint = require('gulp-eslint');
 const rename = require('gulp-rename');
 const sass = require('gulp-sass');
 const source = require('vinyl-source-stream');
+const sourcemaps = require('gulp-sourcemaps');
 const uglify = require('gulp-uglify');
 
 gulp.task('sass', () => {
   return gulp.src('./_assets/scss/app.scss')
+  .pipe(sourcemaps.init())
   .pipe(sass().on('error', sass.logError))
   .pipe(autoprefixer({browsers: ['last 2 versions'], cascade: false}))
   .pipe(cleanCSS())
   .pipe(rename({suffix: '.min'}))
+  .pipe(sourcemaps.write('./'))
   .pipe(gulp.dest('./assets/css'));
 });
 
@@ -37,6 +39,16 @@ gulp.task('trophy', () => {
   .pipe(gulp.dest('./assets/css'));
 });
 
+gulp.task('lint', () => {
+  return gulp.src([
+    './_assets/js/components/_pleaseDontGo.js',
+    './_assets/js/_inits.js'
+  ])
+  .pipe(eslint())
+  .pipe(eslint.format())
+  .pipe(eslint.failAfterError());
+});
+
 gulp.task('browserify', () => {
   return browserify('./_assets/js/app.js')
   .transform('babelify', {presets: ['env']})
@@ -48,10 +60,10 @@ gulp.task('browserify', () => {
   .pipe(gulp.dest('./assets/js'));
 });
 
-gulp.task('build', ['sass', 'fonts', 'browserify']);
+gulp.task('build', ['sass', 'browserify']);
 
 gulp.task('watch', () => {
-  gulp.watch('./_assets/scss/**/*.scss', ['sass', 'fonts']);
+  gulp.watch('./_assets/scss/**/*.scss', ['sass']);
   gulp.watch('./_assets/js/**/*.js', ['browserify']);
 });
 
