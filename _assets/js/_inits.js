e39d3813282e9b79a6568630162c7fb7a9a0f233
thomasvaeth <thomas.vaeth@gmail.com>
2018-05-24 20:25:34 -0700

    Switched Waypoints for AOS.

--- _assets/js/_inits.js
+++ _assets/js/_inits.js
@@ -1,14 +1,82 @@
 // ----------------------------------------------
 // Imports
 // ----------------------------------------------
-import { miscBarba } from './components/_miscellaneous.js';
+import anime from 'animejs';
+import AOS from 'aos';
+import Barba from 'barba.js';
+import InfiniteScroll from './components/_infiniteScroll.js';
+import { miscCycle } from './components/_miscellaneous.js';
 
 // ----------------------------------------------
 // Inits
 // ----------------------------------------------
 $(() => {
 
-  // Inits
-  miscBarba();
+  // Barba
+  const SlideTransition = Barba.BaseTransition.extend({
+    start() {
+      Promise.all([this.newContainerLoading, this.slideOut(), new Promise(resolve => {
+        setTimeout(() => {
+          resolve();
+        }, 700);
+      })]).then(this.slideIn.bind(this));
+    },
+
+    slideOut() {
+      $('.transition').addClass('transition-out');
+
+      return new Promise(resolve => {
+        anime({
+          targets: '.transition',
+          translateX: '-=100%',
+          easing: 'easeInQuart',
+          duration: 400,
+          complete() {
+            resolve();
+          }
+        });
+      });
+    },
+
+    slideIn() {
+      $('.transition').removeClass('transition-out').addClass('transition-in');
+
+      const _this = this;
+
+      window.scrollTo(0, 0);
+      this.oldContainer.style.display = 'none';
+      this.newContainer.style.visibility = 'visible';
+
+      anime({
+        targets: '.transition',
+        translateY: '-100%',
+        easing: 'easeOutQuart',
+        duration: 350,
+        complete() {
+          _this.done();
+        }
+      });
+    }
+  });
+
+  Barba.Dispatcher.on('transitionCompleted', () => {
+    $('.transition').removeClass('transition-in');
+
+    AOS.init({
+      duration: 400,
+      easing: 'ease',
+      once: true
+    });
+
+    miscCycle();
+
+    if ($('.posts').length && $('.posts__next').length) {
+      InfiniteScroll.init();
+    }
+  });
+
+  Barba.Pjax.getTransition = () => SlideTransition;
+
+  Barba.Pjax.start();
 
 });
--- _assets/js/components/_miscellaneous.js
+++ _assets/js/components/_miscellaneous.js
@@ -1,76 +1,6 @@
 // ----------------------------------------------
 // Imports
 // ----------------------------------------------
-import anime from 'animejs';
-import Barba from 'barba.js';
-import waypoint from '../../../node_modules/waypoints/lib/jquery.waypoints.js';
-import InfiniteScroll from './_infiniteScroll.js';
-
-// ----------------------------------------------
-// Barba
-// ----------------------------------------------
-const miscBarba = () => {
-  const SlideTransition = Barba.BaseTransition.extend({
-    start() {
-      Promise.all([this.newContainerLoading, this.slideOut(), new Promise(resolve => {
-        setTimeout(() => {
-          resolve();
-        }, 700);
-      })]).then(this.slideIn.bind(this));
-    },
-
-    slideOut() {
-      $('.transition').addClass('transition-out');
-
-      return new Promise(resolve => {
-        anime({
-          targets: '.transition',
-          translateX: '-=100%',
-          easing: 'easeInQuart',
-          duration: 400,
-          complete() {
-            resolve();
-          }
-        });
-      });
-    },
-
-    slideIn() {
-      $('.transition').removeClass('transition-out').addClass('transition-in');
-
-      const _this = this;
-
-      window.scrollTo(0, 0);
-      this.oldContainer.style.display = 'none';
-      this.newContainer.style.visibility = 'visible';
-
-      anime({
-        targets: '.transition',
-        translateY: '-100%',
-        easing: 'easeOutQuart',
-        duration: 350,
-        complete() {
-          _this.done();
-        }
-      });
-    }
-  });
-
-  Barba.Dispatcher.on('transitionCompleted', () => {
-    $('.transition').removeClass('transition-in');
-
-    miscCycle();
-    miscWaypoints();
-
-    if ($('.posts').length && $('.posts__next').length) {
-      InfiniteScroll.init();
-    }
-  });
-
-  Barba.Pjax.getTransition = () => SlideTransition;
-
-  Barba.Pjax.start();
-};
 
 // ----------------------------------------------
 // Time
@@ -105,25 +35,8 @@ const miscCycle = () => {
 };
 
 // ----------------------------------------------
-// Waypoints
-// ----------------------------------------------
-const miscWaypoints = () => {
-  // Work
-  $('.work__work, .posts__post').each((idx, ele) => {
-    $(ele).waypoint({
-      handler() {
-        $(ele).addClass('js-waypoints');
-        this.destroy();
-      },
-      offset: '90%'
-    });
-  });
-};
-
-// ----------------------------------------------
 // Exports
 // ----------------------------------------------
 module.exports = {
-  miscBarba,
   miscCycle
 };
