3371d14247a519d15ba369eb54f45d0c4573a3e1
thomasvaeth <thomas.vaeth@gmail.com>
2019-04-03 21:32:13 -0700

    Attempted to switch to Barba V2.

--- _assets/js/_inits.js
+++ _assets/js/_inits.js
@@ -3,7 +3,7 @@
 // ----------------------------------------------
 import anime from 'animejs';
 import AOS from 'aos';
-import Barba from 'barba.js';
+import barba from '@barba/core';
 import Rellax from 'rellax';
 import SmoothScroll from 'smooth-scroll';
 import InfiniteScroll from './components/_infiniteScroll.js';
@@ -17,103 +17,145 @@ import OpacityScroll from './components/_opacityScroll.js';
 $(() => {
 
   // Barba
-  const SlideTransition = Barba.BaseTransition.extend({
-    start() {
-      Promise.all([this.newContainerLoading, this.slideOut(), new Promise(resolve => {
-        setTimeout(() => {
-          resolve();
-        }, 1200);
-      })]).then(this.slideIn.bind(this));
-    },
-
-    slideOut() {
-      $('.transition').addClass('transition-out');
-
-      return new Promise(resolve => {
+  barba.init({
+    transitions: [{
+      appear() {
         anime({
           targets: '.transition',
-          translateY: '-=100%',
-          easing: 'easeInQuart',
+          translateY: '-100%',
+          easing: 'easeOutQuart',
           duration: 600,
+          delay: 400,
           complete() {
-            resolve();
+            $('.transition').removeClass('transition-in');
+            $('.transition').css('transform', 'translateY(100%)');
           }
         });
-      });
-    },
 
-    slideIn() {
-      $('.transition').removeClass('transition-out').addClass('transition-in');
+        // AOS.init({
+        //   duration: 1000,
+        //   easing: 'ease',
+        //   once: true
+        // });
 
-      window.scrollTo(0, 0);
-      this.oldContainer.style.display = 'none';
-      this.newContainer.style.visibility = 'visible';
+        // if ($('.rellax').length) {
+        //   const rellax = new Rellax('.rellax');
+        // }
 
-      this.done();
-    }
-  });
+        // const scroll = new SmoothScroll('a[href*="#"]');
 
-  Barba.Dispatcher.on('transitionCompleted', () => {
-    $('body').removeClass('js-scrolling-down js-scrolling-up');
+        // NavigationScroll.init();
+        // OpacityScroll.init();
+        // miscAnchor();
+        // miscCycle();
+        // miscNavigation();
 
-    anime({
-      targets: '.transition',
-      translateY: '-100%',
-      easing: 'easeOutQuart',
-      duration: 600,
-      delay: 400,
-      complete() {
-        $('.transition').removeClass('transition-in');
-        $('.transition').css('transform', 'translateY(100%)');
-      }
-    });
+        // if ($('.posts').length && $('.posts__next').length) {
+        //   InfiniteScroll.init();
+        // }
+      },
+      leave({ current, next, trigger }) {
+        $('.transition').addClass('transition-out');
 
-    AOS.init({
-      duration: 1000,
-      easing: 'ease',
-      once: true
-    });
-
-    if ($('.rellax').length) {
-      const rellax = new Rellax('.rellax');
-    }
-
-    const scroll = new SmoothScroll('a[href*="#"]');
-
-    NavigationScroll.init();
-    OpacityScroll.init();
-    miscAnchor();
-    miscCycle();
-    miscNavigation();
-
-    if ($('.contact').length) {
-      $.ajax({
-        url: 'https://api.instagram.com/v1/users/self/media/recent/?access_token=3980752.1677ed0.62bb6a2ad3ef4dc0a6aad768ab8939ab&count=20&callback=?',
-        method: 'GET',
-        dataType: 'jsonp',
-        success(json) {
-          const targetEl = $('#instagram');
-          let article;
-
-          json.data.forEach((data, idx) => {
-            article = targetEl.children('article').eq(idx);
-            article.find('a').attr('href', data.link);
-            article.find('figure').addClass('scale-down').css('background-image', `url(${data.images.standard_resolution.url})`);
+        return new Promise(resolve => {
+          anime({
+            targets: '.transition',
+            translateY: '-=100%',
+            easing: 'easeInQuart',
+            duration: 600,
+            complete() {
+              resolve();
+            }
           });
-        },
-        error(error) {
-          console.error(error);
-        }
-      });
-    }
+        });
+      },
+      enter({ current, next, trigger }) {
+        $('.transition').removeClass('transition-out').addClass('transition-in');
 
-    if ($('.posts').length && $('.posts__next').length) {
-      InfiniteScroll.init();
-    }
+        window.scrollTo(0, 0);
+
+        $('body').removeClass('js-scrolling-down js-scrolling-up');
+
+        return new Promise(resolve => {
+          anime({
+            targets: '.transition',
+            translateY: '-100%',
+            easing: 'easeOutQuart',
+            duration: 600,
+            delay: 1200,
+            complete() {
+              $('.transition').removeClass('transition-in');
+              $('.transition').css('transform', 'translateY(100%)');
+              resolve();
+            }
+          });
+        });
+      }
+    }],
+    views: [{
+      namespace: 'contact',
+      beforeAppear() {
+        console.log('beforeAppear');
+
+        // $.ajax({
+        //   url: 'https://api.instagram.com/v1/users/self/media/recent/?access_token=3980752.1677ed0.62bb6a2ad3ef4dc0a6aad768ab8939ab&count=20&callback=?',
+        //   method: 'GET',
+        //   dataType: 'jsonp',
+        //   success(json) {
+        //     const targetEl = $('#instagram');
+        //     let article;
+
+        //     json.data.forEach((data, idx) => {
+        //       let article = targetEl.children('article').eq(idx);
+        //       article.find('a').attr('href', data.link);
+        //       article.find('figure').addClass('scale-down').css('background-image', `url(${data.images.standard_resolution.url})`);
+        //     });
+        //   },
+        //   error(error) {
+        //     console.error(error);
+        //   }
+        // });
+      },
+      afterAppear() {
+        console.log('afterAppear');
+      },
+      beforeLeave() {
+        console.log('beforeLeave');
+      },
+      afterLeave() {
+        console.log('afterLeave');
+      },
+      beforeEnter() {
+        console.log('beforeEnter');
+      },
+      afterEnter() {
+        console.log('afterEnter');
+      }
+    }]
   });
 
-  Barba.Pjax.getTransition = () => SlideTransition;
+  // barba.hooks.after(() => {
+  //   AOS.init({
+  //     duration: 1000,
+  //     easing: 'ease',
+  //     once: true
+  //   });
 
-  Barba.Pjax.start();
+  //   if ($('.rellax').length) {
+  //     const rellax = new Rellax('.rellax');
+  //   }
+
+  //   const scroll = new SmoothScroll('a[href*="#"]');
+
+  //   NavigationScroll.init();
+  //   OpacityScroll.init();
+  //   miscAnchor();
+  //   miscCycle();
+  //   miscNavigation();
+
+  //   if ($('.posts').length && $('.posts__next').length) {
+  //     InfiniteScroll.init();
+  //   }
+  // });
 
 });
