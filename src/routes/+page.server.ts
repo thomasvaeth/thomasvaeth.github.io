6f7fa9316bbf00f74002950bf349e20ddf041a77
thomasvaeth <thomas.vaeth@gmail.com>
2026-06-14 16:09:45 -0700

    Updated all packages.

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,10 +3,7 @@ import { fetchSpotifyTrack } from '$lib/server/spotify-track';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ fetch }) => {
-  const [letterboxdFeed, spotifyTrack] = await Promise.all([
-    fetchLetterboxdFeed(fetch),
-    fetchSpotifyTrack(fetch),
-  ]);
+  const [letterboxdFeed, spotifyTrack] = await Promise.all([fetchLetterboxdFeed(fetch), fetchSpotifyTrack(fetch)]);
 
   return {
     letterboxdFeed,
